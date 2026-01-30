0f1cba234cec18e1a226a31c836e1adc9fbdd84c
asung213 <aoys201@exeter.ac.uk>
2026-01-30 12:56:40 +0000

    PrintSquares completed with for loop

---
+++ PrintSquares.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+import javax.sound.midi.Sequence;
+
+public class PrintSquares {
+    public static void main(String[] args) {
+
+        // Declare Scanner object
+        Scanner in = new Scanner(System.in);
+
+        // Prompt for input
+        // System.out.print("Enter your name: ");
+
+        // Read in line from standard input
+        // String name = in.nextLine();
+
+        // System.out.println("Hi " + name + "!");
+
+
+        System.out.print("Please enter the start value: ");
+        // Reads in an int from standard input
+        int startNum = in.nextInt();
+
+        // clear any additonal text that remains uncollected 
+        in.nextLine();
+        
+        System.out.print("Please enter the end value: ");
+        int endNum = in.nextInt();
+
+        System.out.println("You entered: " + startNum + " and " + endNum);
+        System.out.println("Sequence of squares from " + startNum + " to " + endNum);
+        for (int i = startNum; i <= endNum; i++){
+            if (i == endNum){
+                System.out.println(i*i + " ");
+            }
+            else{
+                System.out.print(i*i + " ");
+            }
+        }
+    }
+    
+}
